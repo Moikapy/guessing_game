d700e194c765a6cfcd4232c2217d6f3056c920b6
moikapy <moikapy@devmoi.com>
2022-10-21 17:07:16 -0400

    add loop for guessing until user wins

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 fn main() {
-    let x = 5; // immutable; Points Rewarded: 5
-
     // Ask the user to guess a number
     println!("Guess the number!");
     let secert_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let mut guess = String::new();
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secert_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secert_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
